990ea73cf3393d093c2abc30307b9f4f9289aa1a
Ansh Jain <anshjain722@gmail.com>
2023-10-14 18:38:47 +0530

    fixes

--- Binary_sort/Recursive/recursive_binary.py
+++ Binary_sort/Recursive/recursive_binary.py
@@ -1,4 +1,4 @@
-def recursive_binaruy_search(list, target):
+def recursive_binary_search(list, target):
     if len(list) == 0:
         return False
     else:
@@ -8,16 +8,16 @@ def recursive_binaruy_search(list, target):
             return True
         else:
             if list[midpoint] < target:
-                return recursive_binaruy_search(list[midpoint+1:], target)
+                return recursive_binary_search(list[midpoint+1:], target)
             else:
-                return recursive_binaruy_search(list[:midpoint], target)
+                return recursive_binary_search(list[:midpoint], target)
 
 def verify(result):
     print("Target found:",result)
 
 list = [1,2,3,4,5,6,7,8,9]
-result = recursive_binaruy_search(list, 7)
+result = recursive_binary_search(list, 7)
 verify(result)  
 
-result = result = recursive_binaruy_search(list, 6)
+result = result = recursive_binary_search(list, 6)
 verify(result)
