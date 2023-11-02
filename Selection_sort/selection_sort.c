e184b33200b416d282e9f3452a63a8b8a4346d32
The-Coder-AJ <anshjain722@gmail.com>
2023-11-03 02:28:08 +0530

    fixes

--- Selection_sort/selection_sort.c
+++ Selection_sort/selection_sort.c
@@ -1,11 +1,28 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main(){
-    int list[] = {2,5,3,63,36,34,25,72};
-    int n = sizeof(list)/sizeof(list[0]);
-    int key = 0;
+void selection_sort(int arr[], int n) {
+    int i, j, min_index, temp;
+    for (i = 0; i < n - 1; i++) {
+        min_index = i;
+        for (j = i + 1; j < n; j++) {
+            if (arr[j] < arr[min_index]) {
+                min_index = j;
+            }
+        }
+        temp = arr[i];
+        arr[i] = arr[min_index];
+        arr[min_index] = temp;
+    }
+}
 
-    for
-
-
+int main() {
+    int arr[] = {64, 25, 12, 22, 11};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    selection_sort(arr, n);
+    printf("Sorted array: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+    return 0;
 }
